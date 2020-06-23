688e54ce29f6bee14923a22ed6ed93a202162d75
Yap Hong Kheng <hongkheng.yap@gmail.com>
2020-06-23 18:26:58 +0800

    Responsive gifs for different screen sizes

--- src/components/GIFView.js
+++ src/components/GIFView.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
+import styles from "./GIFView.module.css";
 
 /**
  * GIF window to load a single gif
- * @param url 
- * @param alt
- * @param width
- * @param height
+ * @param sourceSetMedium - for mobil, tablet
+ * @param sourceSetLarge - for desktop
+ * @param url - fallback url
+ * @param alt - image description
  */
 function GIFView(props) {
-  const { url, alt, width, height } = props
+  const { url, alt, sourceSetMedium, sourceSetLarge } = props;
   return (
-    <div>
-      <img src={url} alt={alt} width={width} height={height} />
-    </div>
-  )
+    <picture>
+      <source srcSet={sourceSetLarge.url} media="(min-width: 681px)" />
+      <source
+        className={styles.responsive}
+        srcSet={sourceSetMedium.url}
+        media="(max-width: 680px)"
+      />
+      <img className={styles.responsive} srcSet={url} alt={alt} />
+    </picture>
+  );
 }
 
-export default GIFView
+export default GIFView;
